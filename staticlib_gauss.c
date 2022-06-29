68da5d161da62148c93656799dfe4fa88dacbce9
Sebastian Käwel <sebastian.kaewel@stud.thga.de>
2022-06-30 01:29:36 +0200

    Fehler behoben

--- staticlib_gauss.c
+++ staticlib_gauss.c
@@ -1,18 +1,41 @@
-/*staticlib_gass.c*/
-
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
 
-
 void print_gaussian_erwartung_stdabw(int anzahl,FILE*Input_stream){
 
 
-int laenge;
-
+int laenge=30;
+double temp, temp_var,summe,erwartungw,varianz,stdabw;
 char speicher [laenge];
+char end;
 
-	fgets(speicher,laenge,Input_stream);
-	puts(speicher);
+	while(fgets(speicher, laenge, Input_stream)){				
+			temp=(strtod(speicher, &end))+temp;					//Summe aller Werte
+			temp_var=(pow((strtod(speicher, &end)),2.0))+temp_var;
+		}
+			
+		summe=temp;
+		printf("Summe aller Werte: %f\n",summe);
 
-}
+
+
+		erwartungw=summe/anzahl;							//Berechnen des Erwartungswertes
+		printf("Erwartungswert: %f\n",erwartungw);
+
+											//Varianz							
+	
+
+		varianz=(temp_var/anzahl)-(pow(erwartungw,2.0));
+		printf("Varianz: %f\n",varianz);
+
+												//Standartabweichung
+												//
+												//
+												//
+		stdabw=sqrt(varianz);										//Berechnen der Standardabweichung
+		printf("Standardabweichung: %f\n",stdabw);
+											
+
+
+}	

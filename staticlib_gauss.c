5a2c7ffe47190a404e8f05078970f54f569490b2
Sebastian Käwel <sebastian.kaewel@stud.thga.de>
2022-06-29 23:43:08 +0200

    Erster Versuch

---
+++ staticlib_gauss.c
@@ -0,0 +1,18 @@
+/*staticlib_gass.c*/
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+
+void print_gaussian_erwartung_stdabw(int anzahl,FILE*Input_stream){
+
+
+int laenge;
+
+char speicher [laenge];
+
+	fgets(speicher,laenge,Input_stream);
+	puts(speicher);
+
+}

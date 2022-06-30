475193c693f6176dd10cd7a9e35719d25b54c661
Sebastian Käwel <sebastian.kaewel@stud.thga.de>
2022-06-30 23:25:19 +0200

    Erneuert

--- calc_GAUSS_erwartungsw_stdabw.c
+++ calc_GAUSS_erwartungsw_stdabw.c
@@ -14,24 +14,26 @@ int option,std_abw,erwart_wert,anzahl=100;
 	
 	
 	
-//while((option = getopt(argc, argv, "h")) != -1) 
-//	{
-  //  	    switch(option) 
-//	            	{ 
-//			case 'h':
-//			help();
-//	  		break;	
-//	        	}
-  //  	}
-	FILE*Input_stream;
-	Input_stream = fopen("zufall.txt", "r"); 
-	if(Input_stream== NULL){
-		printf("Datei nicht gefunden/Kann keine Werte einlesen\n");
-		exit(-1);
-	}
-	else{
+while((option = getopt(argc, argv, "h")) != -1) 
+	{
+    switch(option) 
+	    { 
+	case 'h':
+		printf("Version: 0.1 Alpha\n");
+		printf("Programmierer: Roman Rückert und Sebastian Käwel\n");
+		printf("Das Programm erstellt eine Statistik über einen übergebene Datei mit dem Namen zufall.txt im aktuellen Verzeichnis\n");
+		break;	
+        	}
+  	}
+FILE*Input_stream;
+Input_stream = fopen("zufall.txt", "r"); 
+if(Input_stream== NULL){
+printf("Datei nicht gefunden/Kann keine Werte einlesen\n");
+exit(-1);
+}
+else{
 		print_gaussian_erwartung_stdabw(100,Input_stream);
-	}
+}
 fclose(Input_stream);
-
+return 0;
 }
